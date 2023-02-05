8c98fd4b141f9808330cb6ae64af17ad8c059328
Joaquim Gifre Rosales <jgifrerosales@gmail.com>
2023-02-05 19:40:24 +0100

    Basic template to work

---
+++ app-portfolio/src/app/section-me/section-me.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-me',
+  templateUrl: './section-me.component.html',
+  styleUrls: ['./section-me.component.scss']
+})
+export class SectionMeComponent {
+
+}
